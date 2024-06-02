df8b8fb3547a4f94daa2be9d0e4815b724d1e5cf
hajinlee93 <hajin93040@gmail.com>
2024-06-02 20:36:45 +0900

    push

---
+++ data/projects/projectDetailData.ts/project3.ts
@@ -0,0 +1,34 @@
+export const project3 = {
+  title: "이하진의 포트폴리오",
+  intro: "이하진의 포트폴리오",
+  description: `프로젝트들을 정리하고 스킬을 정리해 놓은 프로젝트입니다.`,
+  points: [
+    `\`Next.js\` 를 기반으로 vercel로 배포까지 한 포트폴리오 입니다.`
+  ],
+  contribution: [
+    {
+      title: `간단한 자기 소개, 사용할 수 있는 스킬, 개인적인 프로젝트를 정리했습니다.`,
+      detail: [
+        `사진 아래 연락할 수 있는 이메일과 깃허브 주소를 첨부하였습니다.`,
+        `각 메뉴로 갈 수 있는 간만한 네비게이션을 배치하였습니다.`,
+      ],
+    },
+    {
+      title: `프로젝트 상세 팝업을 구현하였습니다.`,
+      detail: [
+        `각 프로젝트의 상세를 볼 수 있는 팝업을 구성하였습니다.`,
+        `싱세 팝업에서는 프로젝트 내 기능을 간단히 볼 수 있는 사진들을 첨부하였습니다.`,
+      ],
+    },
+  ],
+  startDate: "2024-06-02",
+  endDate: "지속 업데이트 중",
+  category: ["Personal"],
+  path: "portfolio",
+  featured: true,
+  stacks: ["nextjs", "typescript", "tailwindcss", "vercel",],
+  githubUrl: "https://github.com/hajinlee93/shop",
+  docsUrl: "",
+  serviceUrl: "https://hajin.vercel.app/",
+  images: 4,
+};
--- data/projects/projectDetailData.ts/projectDetailData.ts
+++ data/projects/projectDetailData.ts/projectDetailData.ts
@@ -1,8 +1,10 @@
 import { Project } from "@/types/project";
 import { project1 } from "./project1";
 import { project2 } from "./project2";
+import { project3 } from "./project3";
 
 export const projectDetailData: Project[] = [
   project1,
   project2,
+  project3,
 ];
