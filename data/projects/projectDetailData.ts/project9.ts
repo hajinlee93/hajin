99713b522a55884cb7a4a85975a434d10535192d
hajinlee93 <hajin93040@gmail.com>
2024-06-02 18:27:02 +0900

    push

---
+++ data/projects/projectDetailData.ts/project9.ts
@@ -0,0 +1,21 @@
+export const project9 = {
+  title: "포트폴리오",
+  intro: "프론트엔드 포트폴리오 사이트",
+  description:
+    "프론트엔드 개발자를 준비하면서 진행했던 프로젝트, 프론트엔드 개발 스택 등을 한번에 확인할 수 있는 포트폴리오 사이트를 구현하였습니다. 편리한 탐색을 위해 헤더 메뉴 클릭과 스크롤 이동이 가능하도록 구현하여 각각의 프로젝트를 클릭하면 세부 내용을 확인할 수 있는 모달창이 나타납니다.",
+  points: [
+    `타이핑 효과를 주는 커스텀 훅을 만들어 로고에 타이핑 효과를 적용하고, \`FramerMotion\`을 사용하여 슬롯 머신 효과를 구현.`,
+    "이메일, 제목, 본문만 입력하면 간편하게 이메일을 보낼 수 있는 기능 구현.",
+    "다양한 기기나 화면의 크기에 따라 자동으로 맞춤 렌더링이 되도록 반응형 웹 디자인을 구현함.",
+  ],
+  startDate: "2023-11-09",
+  endDate: "진행중",
+  category: ["Personal"],
+  path: "portfolio-site",
+  featured: true,
+  stacks: ["nextjs", "typescript", "tailwindcss", "vercel"],
+  githubUrl: "https://github.com/eeeyooon/portfolio",
+  docsUrl: "",
+  serviceUrl: "https://portfolio-eeeyooon.vercel.app/",
+  images: 4,
+};
