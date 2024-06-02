20c6f94d8c247f3288b5546f1aade3b664bca2ff
hajinlee93 <hajin93040@gmail.com>
2024-06-02 20:15:28 +0900

    push

---
+++ data/projects/projectDetailData.ts/project2.ts
@@ -0,0 +1,36 @@
+export const project2 = {
+  title: "Spring 기반 쇼핑몰 클론 프로젝트",
+  intro: "Spring 기반 쇼핑몰 클론 프로젝트",
+  description: `2인 개발 중인 Spring 기반 쇼핑몰 클론 프로젝트입니다.`,
+  points: [
+    `\`Spring\` 프레임워크 기반으로 로그인 부터 구현 중인 쇼핑몰 프로젝트 입니다.`
+  ],
+  contribution: [
+    {
+      title: `Spring 프레임워크 기반으로 기존 쇼핑몰 페이지를 클론하는 프로젝트입니다.`,
+      detail: [
+        `쇼핑몰 메뉴 중 기능 구현을 위해 퍼블을 나눠 클론하였습니다.`,
+        `페이지마다 보여줄 수 있는 기능에 맞춰 페이지를 나눠 파일구조를 신경써 퍼블을 뜯었습니다.`,
+      ],
+    },
+    {
+      title: `회원가입에 맞는 기능을 가지고 구현했습니다.`,
+      detail: [
+        `필수 항목들의 Null 체크 기능을 함수화하였습니다.`,
+        `Swal 팝업을 사용해 투박하지 않게 팝업을 띄웠습니다.`,
+        `다음 주소 찾기 api를 사용하여 주소를 찾아 넣을 수 있도록 구현했습니다.`,
+
+      ],
+    },
+  ],
+  startDate: "2024-05-23",
+  endDate: "진행중",
+  category: ["Team"],
+  path: "shop",
+  featured: true,
+  stacks: ["java", "javascript", "spring", "oracle",],
+  githubUrl: "https://github.com/hajinlee93/shop",
+  docsUrl: "",
+  serviceUrl: "",
+  images: 8,
+};
